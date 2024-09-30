863aeb21b5eba16fee5a3fd5890030dbbeaf70a9
Yash Bhatt <0yashbhatt0@gmail.com>
2024-09-30 23:22:49 +0530

    Complete Header and Navbar

--- js/app.js
+++ js/app.js
@@ -1,8 +1,36 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close');
+
+/*===============  Menu Show  ===============  */
+// Validate if constant exists 
+
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    });
+}
 
 
+/*=============== MENU HIDE =============== */
+
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
+}
 /*=============== REMOVE MENU MOBILE ===============*/
 
+const navLink = document.querySelectorAll('.nav__link');
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu');
+    // remove the show menu class on each click on nav links
+    navMenu.classList.remove('show-menu');
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
 
 /*=============== ADD BLUR TO HEADER ===============*/
 
