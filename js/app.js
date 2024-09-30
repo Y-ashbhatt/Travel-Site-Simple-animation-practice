122cc4f291640dbcc74e14a22ea266c72e3edb52
Yash Bhatt <0yashbhatt0@gmail.com>
2024-10-01 00:01:09 +0530

    Add header background

--- js/app.js
+++ js/app.js
@@ -1,44 +1,48 @@
 /*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu'),
-    navToggle = document.getElementById('nav-toggle'),
-    navClose = document.getElementById('nav-close');
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
 /*===============  Menu Show  ===============  */
-// Validate if constant exists 
+// Validate if constant exists
 
-if(navToggle){
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.add('show-menu');
-    });
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
 }
-
 
 /*=============== MENU HIDE =============== */
 
-if(navClose){
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu');
-    });
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
 }
 /*=============== REMOVE MENU MOBILE ===============*/
 
-const navLink = document.querySelectorAll('.nav__link');
+const navLink = document.querySelectorAll(".nav__link");
 
-const linkAction = () =>{
-    const navMenu = document.getElementById('nav-menu');
-    // remove the show menu class on each click on nav links
-    navMenu.classList.remove('show-menu');
-}
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  // remove the show menu class on each click on nav links
+  navMenu.classList.remove("show-menu");
+};
 
-navLink.forEach(n => n.addEventListener('click', linkAction));
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== ADD BLUR TO HEADER ===============*/
+const blurHeader = () => {
+  const header = document.getElementById("header");
+  // when scroll is greater than 50 viewport height add class blur header
+  this.scrollY >= 50
+    ? header.classList.add("blur-header")
+    : header.classList.remove("blur-header");
+};
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+window.addEventListener("scroll", blurHeader);
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
